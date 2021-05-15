f0a6edb7a44fcfb6a432ea1826240e4e085648ad
sonpxp <sonmob202@gmail.com>
2021-05-15 11:30:12 +0700

    adapter-rxjava3

--- app/src/main/java/com/sonmob/movieapp/network/ApiClient.java
+++ app/src/main/java/com/sonmob/movieapp/network/ApiClient.java
@@ -1,6 +1,7 @@
 package com.sonmob.movieapp.network;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
@@ -12,8 +13,10 @@ public class ApiClient {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
+
         }
         return retrofit;
     }
